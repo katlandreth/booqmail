09e86386065a804762394c35d1703f51d7cde110
Kat Landreth <katlandreth@gmail.com>
2016-10-10 16:23:32 -0500

    sends os version ids through form, then finds the os version by id in the contact_message mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -3,6 +3,8 @@ class Mailer < ApplicationMailer
 
   def contact_me(message)
     @message = message
+    @os_version = OsVersion.find(@message.os_version_id)
+    @operating_system = OperatingSystem.find(@message.operating_system_id)
     if message.image
       attachment_name = message.image.original_filename
       attachments[attachment_name] = message.image.read
