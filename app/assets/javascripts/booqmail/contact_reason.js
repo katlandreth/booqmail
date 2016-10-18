994d5893bc01e03e353324491cc4a82c58c98bac
Kat Landreth <katlandreth@gmail.com>
2016-10-18 16:50:45 -0500

    re-writes reason js to better account for each case

--- app/assets/javascripts/booqmail/contact_reason.js
+++ app/assets/javascripts/booqmail/contact_reason.js
@@ -1,12 +1,50 @@
 $(document).ready(function(){
+
   $("#reason").on("change", function(){
-    if ($(this).val() == "gimp help") {
-      console.log($(this).val());
-      $(".system-info").removeClass("contact-hide").addClass("contact-show");
+    adaptForm($(this).val());
+  });
+
+  var adaptForm = function(reason) {
+    switch(reason) {
+      case "gimp help":
+        adaptSectionVisibility(".system-info, .better-question-prompt", "show");
+        adaptHelpText("gimp help", "show");
+        break;
+
+      case "purchase help":
+        adaptSectionVisibility(".system-info, .contact-alert", "hide");
+        adaptSectionVisibility(".better-question-prompt", "show");
+        adaptHelpText("purchase help", "show");
+        break;
+
+      case "business":
+        adaptSectionVisibility(".system-info, .better-question-prompt, .contact-alert", "hide");
+        adaptHelpText("", "hide");
+        break;
+
+      case "other":
+        adaptSectionVisibility(".system-info, .better-question-prompt, .contact-alert", "hide");
+        adaptHelpText("", "hide");
+        break;
+    }
+  };
+
+  var adaptSectionVisibility = function(section, visibility) {
+    if (visibility == "show") {
+      $(section).removeClass("contact-hide").addClass("contact-show");
     }
     else {
-      console.log($(this).val());
-      $(".system-info").removeClass("contact-show").addClass("contact-hide");
+      $(section).removeClass("contact-show").addClass("contact-hide");
     }
-  });
+  };
+
+  var adaptHelpText = function(reason, visibility) {
+    if (visibility == "show") {
+      $("#better-question-reason").text(reason);
+    }
+    else {
+      $("#better-question-reason").text("");
+    }
+  };
+
 });
